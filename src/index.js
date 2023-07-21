7dab3299ed9b83f54ac1c627aa2ba437e948e142
Satont <satontworldwide@gmail.com>
2023-07-21 04:59:47 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require("./app.js");
-const db = require("./db.js");
+const mongoose = require('mongoose')
+
 const port = 3000;
 
 async function start() {
-  console.log("Connected successfully to server");
-  express.listen(port, () => {
-    console.log(`http://localhost:3000/`);
-  });
+  try {
+    await mongoose.connect(`mongodb://localhost:27017/authentification-express`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    express.listen(port, () => {
+      console.log(`http://localhost:3000/`);
+    });
+  } catch (error) {
+    console.log(error)
+  }
 }
 start();
